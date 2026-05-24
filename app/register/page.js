60a3dd2e9115542a127d57e4fcf5ecb881bc0c11
mdfizz655 <info.mdfizz65@gmail.com>
2026-05-24 22:53:46 +0600

    update

--- app/register/page.js
+++ app/register/page.js
@@ -7,7 +7,7 @@ import { toast } from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
 import { FiEye, FiEyeOff, FiCheckCircle, FiXCircle } from "react-icons/fi";
 
-// পাথগুলো ম্যানুয়ালি ঠিক করা হলো যাতে Vercel বিল্ড ফেইল না করে
+
 import { api } from "../../lib/axios"; 
 
 function Rule({ ok, text }) {
